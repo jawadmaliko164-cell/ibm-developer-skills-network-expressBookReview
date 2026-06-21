e97b434bd61caea74841862a9638061bc920caea
SampavyKangeyan <sam990731@gmail.com>
2026-06-22 05:23:15 +0530

    Task 5 is done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,8 +52,20 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbnNo = req.params.isbn;
+  const book = books[isbnNo];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found." });
+  }
+
+  const reviews = book.reviews;
+
+  if (!reviews || Object.keys(reviews).length === 0) {
+    return res.json({ message: "No reviews found for this book." });
+  }
+
+  return res.json(reviews);
 });
 
 module.exports.general = public_users;
