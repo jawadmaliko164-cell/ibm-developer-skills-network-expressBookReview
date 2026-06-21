c7512dfe3b104d020b0c5eb7b4f2db382612b5b3
SampavyKangeyan <sam990731@gmail.com>
2026-06-22 05:14:32 +0530

    Task 4 is done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,16 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const titleName = req.params.title;
+  const result = {};
+
+  Object.keys(books).forEach((key) => {
+    if (books[key].title === titleName) {
+      result[key] = books[key];
+    }
+  });
+
+  res.json(result);
 });
 
 //  Get book review
